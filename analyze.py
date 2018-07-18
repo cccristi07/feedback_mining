d3651b779c2510d8214a8014b280771330c6b780
Cristi <cccristi07@gmail.com>
2018-07-18 10:54:50 +0300

    finished

--- analyze.py
+++ analyze.py
@@ -6,8 +6,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.snowball import RomanianStemmer
-
-from sklearn.feature_extraction.text import CountVectorizer
+from nltk import word_tokenize
+from wordcloud import WordCloud
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import matplotlib.pyplot as plt
 import csv
 import spacy
@@ -23,7 +24,9 @@ specs = ["B1", "B2", "B3"]
 
 rom_sw = set(stopwords.words("romanian"))
 
-print("{} gave their opinions on this form")
+rom_sw.add("și")
+
+print("{} studenti au participat la formular".format(no_studs))
 
 # no of students from each directions
 studs_spec = {}
@@ -64,9 +67,16 @@ classes = {}
 for spec in specs:
     cls_data = list(df[df.Dir == spec].Class)
     tokenizer = RegexpTokenizer(r'\w+')
-    cls_words = tokenizer.tokenize(" ".join(cls_data))
+    cls_words = []
+    for c in cls_data:
+        if c is "":
+            continue
+        tokens = tokenizer.tokenize(c)
+        tokens = " ".join(list(filter(lambda x: x not in rom_sw, tokens)))
+        cls_words.append(tokens)
+
     cls_words = [w for w in cls_words if w not in rom_sw]
-    cv = CountVectorizer(min_df=1)
+    cv = CountVectorizer(min_df=1, ngram_range=(1, 3))
     scor = cv.fit_transform(cls_words).toarray()
     scor = scor.sum(axis=0) # obtinem cele mai importante coloane
     scor = list(zip(scor, cv.get_feature_names()))
@@ -81,25 +91,76 @@ for spec in specs:
     abuse_data = [ad for ad in abuse_data if 'nu' not in ad.lower()]
     abuse[spec] = len(abuse_data)
     proc = abuse[spec] / studs_spec[spec] * 100
-    print("{0:.2f}% dintre studentii de la {1} raporteaza abuzuri ale profesorilor".format(proc, spec))
+    print("{0:.2f}% dintre studentii de la {1} raporteaza abuzuri din partea profesorilor".format(proc, spec))
 
 # bad profs for each direction
 profs = {}
 for spec in specs:
     prof_data = list(df[df.Dir == spec].Prof)
     tokenizer = RegexpTokenizer(r'\w+')
-    cls_words = tokenizer.tokenize(" ".join(prof_data))
-    cls_words = [w for w in cls_words if w not in rom_sw]
-    cv = CountVectorizer()
-    scor = cv.fit_transform(cls_words).toarray()
+    prof_words = []
+    for p in prof_data:
+        if p is "":
+            continue
+        tokens = tokenizer.tokenize(p)
+        tokens = " ".join(list(filter(lambda x: x not in rom_sw, tokens)))
+        prof_words.append(tokens)
+
+    cv = CountVectorizer(min_df=1, ngram_range=(1, 3))
+    scor = cv.fit_transform(prof_words).toarray()
     scor = scor.sum(axis=0) # obtinem cele mai importante coloane
     scor = list(zip(scor, cv.get_feature_names()))
     scor = sorted(scor, key=lambda x: x[0], reverse=True)
-    print(scor)
+    profs[spec] = [prof for _, prof in scor[:5]]
+    print("Studentii de la {} reclama probleme de la {}".format(spec,
+                                                                profs[spec]))
 
 # what workload did the directions consider
+work_load = {}
+for spec in specs:
+    wl_data = list(df[df.Dir == spec].Difficulty)
+    tokenizer = RegexpTokenizer(r'\w+')
+    wl_words = []
+    for w in wl_data:
+        if w is "":
+            continue
+        tokens = tokenizer.tokenize(w)
+        tokens = " ".join(list(filter(lambda x: x not in rom_sw, tokens)))
+        wl_words.append(tokens)
+
+    cv = CountVectorizer(min_df=2, ngram_range=(1, 3))
+    scor = cv.fit_transform(wl_words).toarray()
+    scor = scor.sum(axis=0)
+    scor = list(zip(scor, cv.get_feature_names()))
+    scor = sorted(scor,key=lambda x: x[0], reverse=True)
+    work_load[spec] = [wl for(_, wl) in scor[:5]]
+    proc = scor[0][0] / studs_spec[spec] * 100
+    print("pentru {0} încărcarea a fost {1} in {2:.2f}% din stud".format(spec, scor[0][1], proc))
+
+
 
 # what were the top words / topics for each direction
+top_words = {}
+global_words = []
+for spec in specs:
+    # preprocess data and turn into a wordcloud
+    ops = list(df[df.Dir == spec].Opinion)
+    ops = [op for op in ops if op is not ""]
+
+    txt = " ".join(ops)
+    tokens = tokenizer.tokenize(txt)
+    tokens = [t for t in tokens if t.lower() not in rom_sw]
+    txt = " ".join(tokens)
+    global_words.append(txt.lower())
+    wc = WordCloud(width=900, height=400).generate(txt)
+    top_words[spec] = txt
+    plt.imshow(wc)
+    plt.savefig("{}top_words.png".format(spec))
+
 
 
 # global analysis for all directions
+global_words = " ".join(global_words)
+wc = WordCloud(width=900, height=400).generate(global_words)
+plt.imshow(wc)
+plt.savefig("all_specs.png")
